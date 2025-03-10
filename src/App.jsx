fc7ebba45b49e8de08d38e4d2bb2d5d5db5caf1b
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2025-03-10 07:52:03 +0600

    In the middle

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./sass/style.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import { useEffect } from "react";
+import React from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { router } from "./routes/router";
